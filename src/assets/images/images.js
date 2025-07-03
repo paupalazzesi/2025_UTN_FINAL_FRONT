0db1798c2be5db90ee525f7f4b850d1f3a9cc42a
Paula Palazzesi <paula.palazzesi@miracledevs.com>
2025-07-03 19:38:22 -0300

    add contact images

---
+++ src/assets/images/images.js
@@ -0,0 +1,23 @@
+import michael from './michael-scott-profile.png'
+import jim from './jim-halpert-profile.png'
+import pam from './pam-beasly-profile.png'
+import dwight from './dwight-schrute-profile.png'
+import angela from './angela-profile.png'
+import stanley from './stanley-profile.png'
+import ryan from './ryan-profile.png'
+import kelly from './kelly-profile.png'
+import creed from './creed-profile.png'
+import kevin from './kevin-profile.png'
+
+export default {
+    michael: michael,
+    dwight: dwight,
+    jim: jim,
+    pam: pam,
+    angela: angela,
+    stanley: stanley,
+    ryan: ryan,
+    kelly: kelly,
+    creed: creed,
+    kevin: kevin,
+}
--- src/data/contact-mook.js
+++ src/data/contact-mook.js
@@ -1,3 +1,5 @@
+import images from '../assets/images/images'
+
 //Este archivo solo contiene la informacion de la app, en futuro lo vamos a eliminar
 const mook_data = {
   contacts: [
@@ -5,7 +7,7 @@ const mook_data = {
       id: 1,
       name: "Michael Scott",
       last_time_connected: "Today at 10:32 AM",
-      img: "https://ui-avatars.com/api/?name=Michael+Scott&background=007AFF&color=fff",
+      img: images.michael,
       last_message: {
         id: 1,
         text: "Hello!",
@@ -25,7 +27,7 @@ const mook_data = {
       id: 2,
       name: "Jim Halpert",
       last_time_connected: "Online",
-      img: "https://ui-avatars.com/api/?name=Jim+Halpert&background=34C759&color=fff",
+      img: images.jim,
       last_message: {
         id: 1,
         text: "Hey there!",
@@ -45,7 +47,7 @@ const mook_data = {
       id: 3,
       name: "Pam Beesly",
       last_time_connected: "Today at 09:18 AM",
-      img: "https://ui-avatars.com/api/?name=Pam+Beesly&background=FF9500&color=fff",
+      img: images.pam,
       last_message: {
         id: 1,
         text: "Hi!",
@@ -65,7 +67,7 @@ const mook_data = {
       id: 4,
       name: "Dwight Schrute",
       last_time_connected: "Today at 10:35 AM",
-      img: "https://ui-avatars.com/api/?name=Dwight+Schrute&background=FF3B30&color=fff",
+      img: images.dwight,
       last_message: {
         id: 1,
         text: "Fact.",
@@ -85,7 +87,7 @@ const mook_data = {
       id: 5,
       name: "Angela Martin",
       last_time_connected: "Today at 08:50 AM",
-      img: "https://ui-avatars.com/api/?name=Angela+Martin&background=AF52DE&color=fff",
+      img: images.angela,
       last_message: {
         id: 1,
         text: "Meow.",
@@ -105,7 +107,7 @@ const mook_data = {
       id: 6,
       name: "Kevin Malone",
       last_time_connected: "Yesterday at 07:00 PM",
-      img: "https://ui-avatars.com/api/?name=Kevin+Malone&background=FFCC00&color=000",
+      img: images.kevin,
       last_message: {
         id: 1,
         text: "Why waste time say lot word",
@@ -125,7 +127,7 @@ const mook_data = {
       id: 7,
       name: "Stanley Hudson",
       last_time_connected: "Today at 08:00 AM",
-      img: "https://ui-avatars.com/api/?name=Stanley+Hudson&background=8E8E93&color=fff",
+      img:  images.stanley,
       last_message: {
         id: 1,
         text: "Did I stutter?",
@@ -145,7 +147,7 @@ const mook_data = {
       id: 8,
       name: "Ryan Howard",
       last_time_connected: "Today at 10:00 AM",
-      img: "https://ui-avatars.com/api/?name=Ryan+Howard&background=5856D6&color=fff",
+      img:  images.ryan,
       last_message: {
         id: 1,
         text: "Entrepreneur 💼📈",
@@ -165,7 +167,7 @@ const mook_data = {
       id: 9,
       name: "Kelly Kapoor",
       last_time_connected: "Online",
-      img: "https://ui-avatars.com/api/?name=Kelly+Kapoor&background=FF2D55&color=fff",
+      img: images.kelly,
       last_message: {
         id: 1,
         text: "Shopping 🛍️✨",
@@ -185,7 +187,7 @@ const mook_data = {
       id: 10,
       name: "Creed Bratton",
       last_time_connected: "Unknown",
-      img: "https://ui-avatars.com/api/?name=Creed+Bratton&background=FF9F0A&color=fff",
+      img: images.creed,
       last_message: {
         id: 1,
         text: "Quality Assurance?",
