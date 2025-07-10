986a897bfeb61e6c465359183f07e5878d2b24e0
Paula Palazzesi <paula.palazzesi@miracledevs.com>
2025-07-10 12:42:50 -0300

    chat ux fixes

--- src/Component/Chat/Chat.jsx
+++ src/Component/Chat/Chat.jsx
@@ -1,23 +1,24 @@
-import React, { useContext } from 'react'
-import Message from '../Message/Message'
-import { MessagesContext } from '../../Context/MessagesContext'
-import './Chat.scss'
+import React, {useContext, useEffect, useRef} from "react";
+import Message from "../Message/Message";
+import {MessagesContext} from "../../Context/MessagesContext";
+import "./Chat.scss";
 
 export default function Chat() {
-    const {messages} = useContext(MessagesContext)
-    
-    return (
-        <div className='chat'>
-            {
-                messages.map((message) => {
-                    return (
-                        <Message 
-                            key={message.id} 
-                            {...message}
-                        />
-                    )
-                })
-            }
-        </div>
-    )
+	const {messages} = useContext(MessagesContext);
+	const chatEndRef = useRef(null);
+
+	useEffect(() => {
+		if (chatEndRef.current) {
+			chatEndRef.current.scrollIntoView({behavior: "smooth"});
+		}
+	}, [messages]);
+
+	return (
+		<div className="chat">
+			{messages.map((message) => {
+				return <Message key={message.id} {...message} />;
+			})}
+			<div ref={chatEndRef} />
+		</div>
+	);
 }
